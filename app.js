4522ebde5d2103c156a5cd19f92d5d5a918b2815
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-02 14:10:02 -0800

    add route for /books/:id

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const { connectToDb, getDb } = require("./db");
 
 // init app & middleware
@@ -29,4 +30,19 @@ app.get("/books", (req, res) => {
     .catch(() => {
       res.status(500).json({ error: "Could not fetch the documents" });
     });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((document) => {
+        res.status(200).json(document);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid document ID" });
+  }
 });
